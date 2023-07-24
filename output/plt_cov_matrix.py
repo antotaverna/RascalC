3716d0d2bd7c51dc0a43b16ddbd534cf24a776d6
antotaverna <ataverna@unc.edu.ar>
2023-07-24 18:41:37 -0300

    update plot

--- output/plt_cov_matrix.py
+++ output/plt_cov_matrix.py
@@ -5,31 +5,63 @@ import seaborn as sns
 ##################################################
 ######################### l0 #####################
 
-data = np.load('nd3_00/Rescaled_Covariance_Matrices_Legendre_n25_l0.npz') #nd3_00 fixedAmp_002
+data = np.load('nd3_00/Rescaled_Covariance_Matrices_Legendre_n25_l0_1x.npz') #nd3_00 fixedAmp_002
 print(data.files)
 
 cov=data.get('full_theory_covariance')
 cov_i=data.get('individual_theory_covariances')
 
+cov_pycorr=np.genfromtxt('nd3_00/pycorr_z0_nd3_00_cov_jkn_8_3_r_25_150.dat')
+
+################### plot ###################
+fig, (ax1,ax2) = plt.subplots(1,2, figsize=(8,4))
+#fig, ((ax1, ax2), (ax3, ax4))  = plt.subplots(2,2)
+
 # the index of the position of yticks
 num_ticks = 25
 ticks = np.linspace(25, 145, num_ticks)
 
-sns.heatmap(cov, cmap="crest", yticklabels=ticks, xticklabels=ticks)
+sns.heatmap(cov, cmap="crest", yticklabels=ticks, xticklabels=ticks,ax=ax1)
+ax1.set_aspect(1)
+sns.heatmap(cov_pycorr, cmap="crest", yticklabels=ticks, xticklabels=ticks,ax=ax2)
+ax2.set_aspect(1)
+
 plt.title('Covariance Matrix nd3_00')
 plt.show()
+##################################################
 
 nbin=25
 R = np.zeros((nbin, nbin))
+R_pycorr = np.zeros((nbin, nbin))
 for i in range(0,nbin):
     for j in range(0,nbin):
         R[i,j] = cov[i,j]/np.sqrt(cov[i,i]*cov[j,j])
+        R_pycorr[i,j] = cov_pycorr[i,j]/np.sqrt(cov_pycorr[i,i]*cov_pycorr[j,j])
 
-sns.heatmap(R, vmin=0, vmax=1, yticklabels=ticks, xticklabels=ticks)
-plt.title(r'Normalised Covariance Matrix nd3_00: $C_{ij}$/$\sqrt{C_{ii}*C_{jj}}$')
+
+################### plot ###################
+fig, (ax1,ax2) = plt.subplots(1,2, figsize=(12,5))
+#fig.subplots_adjust(left=0.07, bottom=0.1, right=None, top=None, wspace=0.5, hspace=0.3)
+#fig, ((ax1, ax2), (ax3, ax4))  = plt.subplots(2,2)
+
+fig.suptitle(r'Normalised Covariance Matrix nd3_00: $C_{ij}$/$\sqrt{C_{ii}*C_{jj}}$')
+# the index of the position of yticks
+num_ticks = 25
+ticks = np.linspace(25, 145, num_ticks)
+
+sns.heatmap(R, vmin=0, vmax=1, cmap="coolwarm", yticklabels=ticks, xticklabels=ticks,ax=ax1)
+ax1.set_aspect(1)
+ax1.set_title('RascalC')
+ax1.set_xlabel('r [Mpc/h]')
+ax1.set_ylabel('r [Mpc/h]')
+sns.heatmap(R_pycorr, vmin=0, vmax=1, cmap="coolwarm", yticklabels=ticks, xticklabels=ticks,ax=ax2)
+ax2.set_aspect(1)
+ax2.set_title('Pycorr')
 plt.xlabel('r [Mpc/h]')
 plt.ylabel('r [Mpc/h]')
+
 plt.show()
+##################################################
 
 
 # 25 bines entre r∈[25,150] sizebin=5 Mpc/h
@@ -47,20 +79,40 @@ err = dcorr[:,2]
 errp = dcorr[:,3]
 
 
-#plt.errorbar(r,r**2*xi,yerr=r**2*err,marker='.', label='pycorr nsv=8^3 (binsize=5 Mpc/h)')
-plt.errorbar(r,r**2*xi,yerr=r**2*error, label='error RascalC')
-#plt.errorbar(r,r**2*xi,yerr=r**2*errp, label='Poisson: (1 + ξ)/√(DD)',color='y')
+################### plot ###################
+fig, (ax1,ax2) = plt.subplots(1,2, figsize=(12,5))
+#fig.subplots_adjust(left=0.07, bottom=0.1, right=None, top=None, wspace=0.5, hspace=0.3)
+#fig, ((ax1, ax2), (ax3, ax4))  = plt.subplots(2,2)
 
+fig.suptitle(r'2pcf nd3_00')
+# the index of the position of yticks
+num_ticks = 25
+ticks = np.linspace(25, 145, num_ticks)
+
+ax1.errorbar(r,r**2*xi,yerr=r**2*error, label='error RascalC (binsize=5 Mpc/h)')
+ax1.errorbar(r,r**2*xi,yerr=r**2*errp, marker='.', markersize=4, linestyle="None", label='Poisson: (1 + ξ)/√(DD)',color='r')
+#ax1.set_aspect(0.5)
+ax1.set_title('RascalC')
+ax1.set_ylim(-80,90)
+ax1.set_xlabel('r [Mpc/h]')
+ax1.set_ylabel(r'$r^2 \xi(r)$')
+ax1.legend()
+
+ax2.errorbar(r,r**2*xi,yerr=r**2*err, marker='.', label='pycorr nsv=8^3 (binsize=5 Mpc/h)')
+#ax2.set_aspect(0.5)
+ax2.set_ylim(-80,90)
+ax2.set_title('Pycorr')
+ax2.set_xlabel('r [Mpc/h]')
+ax2.set_ylabel(r'$r^2 \xi(r)$')
+plt.legend()
+
+plt.show()
+##################################################
 
 dd=np.genfromtxt('nd3_00/xi_to_multipoles.dat')
 r = dd[:,0]
 xi = dd[:,1]
-plt.plot(r,r**2*xi,color='red', linestyle='dotted', label='ξ0 RascalC (binsize=1 Mpc/h) nd3_00')
-
-
-plt.legend()
-plt.show()
-
+#plt.plot(r,r**2*xi,color='red', linestyle='dotted', label='ξ0 RascalC (binsize=1 Mpc/h) nd3_00')
 stop
 
 ###################################
@@ -87,6 +139,8 @@ plt.errorbar(rc,rc**2*xic,yerr=rc**2*error,marker='.', label='RascalC error')
 plt.legend()
 plt.show()
 
+
+stop
 ###########################
 ## Legendre moment l0 y l2
 ###########################
