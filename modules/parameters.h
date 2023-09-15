8953b919f981a16eac822f3af2fa500920f37c45
antotaverna <antotaverna@gmail.com>
2023-09-15 05:43:34 +0200

    mv 10x to local

--- modules/parameters.h
+++ modules/parameters.h
@@ -17,9 +17,9 @@ public:
 	//const char default_fname[500] = "../den_files/nd1_00_randoms_10x_fixedAmp_002.txt";
 	//const char default_fname[500] = "../den_files/nd1_00_randoms_1x_fixedAmp_002.txt";
 	//const char default_fname[500] = "../den_files/nd3_00_randoms_10x_fixedAmp_002.txt";
-	const char default_fname[500] = "../den_files/nd3_00_randoms_1x_fixedAmp_002.txt";
+	//const char default_fname[500] = "../den_files/nd3_00_randoms_1x_fixedAmp_002.txt";
 	//const char default_fname[500] = "../den_files/nd3_00_randoms_2x_fixedAmp_002.txt";
-	//const char default_fname[500] = "../den_files/nd3_00_randoms_5x_fixedAmp_002.txt";
+	const char default_fname[500] = "../den_files/nd3_00_randoms_5x_fixedAmp_002.txt";
 
     // Name of the radial binning .csv file
     char *radial_bin_file = NULL;
@@ -82,9 +82,9 @@ public:
 
     char *phi_file = NULL; // Survey correction function coefficient file
     //const char default_phi_file[500] = "output/nd1_00/BinCorrectionFactor_n25_periodic_11.txt";
-    const char default_phi_file[500] = "output/nd3_00/BinCorrectionFactor_n25_periodic_11_25_150_1x.txt";
+    //const char default_phi_file[500] = "output/nd3_00/BinCorrectionFactor_n25_periodic_11_25_150_1x.txt";
     //const char default_phi_file[500] = "output/nd3_00/BinCorrectionFactor_n25_periodic_11_25_150_2x.txt";
-    //const char default_phi_file[500] = "output/nd3_00/BinCorrectionFactor_n25_periodic_11_25_150_5x.txt";
+    const char default_phi_file[500] = "output/nd3_00/BinCorrectionFactor_n25_periodic_11_25_150_5x.txt";
     //const char default_phi_file[500] = "output/nd3_00/BinCorrectionFactor_n25_periodic_11_25_150_10x.txt";
     //const char default_phi_file[500] = "output/nd3_00/BinCorrectionFactor_n25_periodic_11_60_160_10x.txt";
 
